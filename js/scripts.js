5f49f6c52250c8fca1159f30f2c2a4626c707a5b
Website Admin <>
2021-08-15 15:34:21 -0400

    wbartongames website

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - Small Business v5.0.3 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// Add JavaScript here
